668765aed360794e68afc76b52a78b8bfc12586d
Oskar <oskar.calvo@aeg.eus>
2024-07-24 22:33:28 +0200

    [NEW] helmet, ring, boot and shield models [UPDATE] player model

--- models/playerModel.js
+++ models/playerModel.js
@@ -8,30 +8,41 @@ const { Schema } = mongoose;
 
 
 const playerSchema = new Schema({
-   name: String,
-   email: String,
-   avatar: String,
-   level: {type: Number, default: 1},
-   experience: {type: Number, default: 0},
-   is_active: {type: Boolean, default: true},
-   profile: {type: Schema.Types.ObjectId,    ref: "Profile"},
-   equipment: {armor: {type: Schema.Types.ObjectId, ref: "Armor"},
-               weapon: {type: Schema.Types.ObjectId, ref: "Weapon"},
-               artifact: {type: Schema.Types.ObjectId, ref: "Artifact"},
-               antidote_potion: {type: Schema.Types.ObjectId, ref: "PotionAntidote"},
-               healing_potion: {type: Schema.Types.ObjectId, ref: "PotionHealing"},
-               enhancer_potion: {type: Schema.Types.ObjectId, ref: "PotionEnhancer"}
-               },
-            
-
-   inventory: {type: Array, default: null},
-   tasks: { type:[String], default: null },
-   created_date: { type: Date, default: Date.now },
-   gold: {type: Number, default:50}
-
-
+  name: String,
+  email: String,
+  avatar: String,
+  level: {type: Number, default: 1},
+  experience: {type: Number, default: 0},
+  is_active: {type: Boolean, default: true},
+  profile: {type: Schema.Types.ObjectId, ref: "Profile"},
+  equipment: {
+    helmet: {type: Schema.Types.ObjectId, ref: "Helmet"},
+    weapon: {type: Schema.Types.ObjectId, ref: "Weapon"},
+    armor: {type: Schema.Types.ObjectId, ref: "Armor"},
+    shield: {type: Schema.Types.ObjectId, ref: "Shield"},
+    artifact: {type: Schema.Types.ObjectId, ref: "Artifact"},
+    boot: {type: Schema.Types.ObjectId, ref: "Boot"},
+    ring: {type: Schema.Types.ObjectId, ref: "Ring"},
+    antidote_potion: {type: Schema.Types.ObjectId, ref: "PotionAntidote"},
+    healing_potion: {type: Schema.Types.ObjectId, ref: "PotionHealing"},
+    enhancer_potion: {type: Schema.Types.ObjectId, ref: "PotionEnhancer"}
+    },
+  inventory: {
+    helmet: [{type: Schema.Types.ObjectId, ref: "Helmet"}],
+    weapon: [{type: Schema.Types.ObjectId, ref: "Weapon"}],
+    armor: [{type: Schema.Types.ObjectId, ref: "Armor"}],
+    shield: [{type: Schema.Types.ObjectId, ref: "Shield"}],
+    artifact: [{type: Schema.Types.ObjectId, ref: "Artifact"}],
+    boot: [{type: Schema.Types.ObjectId, ref: "Boot"}],
+    ring: [{type: Schema.Types.ObjectId, ref: "Ring"}],
+    antidote_potion: [{type: Schema.Types.ObjectId, ref: "PotionAntidote"}],
+    healing_potion: [{type: Schema.Types.ObjectId, ref: "PotionHealing"}],
+    enhancer_potion: [{type: Schema.Types.ObjectId, ref: "PotionEnhancer"}]
+  },
+  tasks: { type:[String], default: null },
+  created_date: { type: Date, default: Date.now },
+  gold: {type: Number, default:50}
 });
-
 
 // Exportamos el modelo para usarlo en otros ficheros
 module.exports = mongoose.model('Player', playerSchema);
---
+++ models/ringModel.js
@@ -0,0 +1,27 @@
+// Cargamos el módulo de mongoose
+const mongoose =  require('mongoose');
+
+// Usaremos los esquemas
+const { Schema } = mongoose;
+
+// Creamos el objeto del esquema y sus atributos
+const ringSchema = new Schema({
+   name: String,
+   description: String,
+   type: String,
+   image: String,
+   modifiers: {
+      intelligence: Number,
+      dexterity: Number,
+      constitution: Number,
+      insanity: Number,
+      charisma: Number,
+      strength: Number
+    },
+   min_level: Number,
+   profiles: [{type: Schema.Types.ObjectId, ref: "Profile"}]
+
+});
+
+// Exportamos el modelo para usarlo en otros ficheros
+module.exports = mongoose.model('Ring', ringSchema);
